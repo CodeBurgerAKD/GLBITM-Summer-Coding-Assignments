5bee27909d30c3eb58ff6cfa19af637052f18875
Akshat Dhoundiyal <akshatdh0309@gmail.com>
2026-06-02 23:02:51 +0530

    Add files via upload

---
+++ DAY 2/palindrome.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = (reversedNum * 10) + digit;
+            num = num / 10;
+        }
+
+        if (originalNum == reversedNum) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not Palindrome");
+        }
+        
+        
+    } 
+    
+}
