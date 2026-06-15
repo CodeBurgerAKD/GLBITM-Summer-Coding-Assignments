53512cd8aa7989a358319cb1e8341efb23934cb6
Akshat Dhoundiyal <akshatdh0309@gmail.com>
2026-06-15 22:44:13 +0530

    Implement array reversal in array_rev.java
    This program reverses an array input by the user and prints the reversed array.

---
+++ DAY 15/array_rev.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class array_rev {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            
+            start++;
+            end--;
+        }
+        
+        System.out.println("Reversed array:");
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+
+}
